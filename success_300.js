726355f3ebe3bf4bc388344eff1cb5bb0d139e61
mooncero <97421668+mooncero@users.noreply.github.com>
2022-01-10 10:50:53 +0700

    Add files via upload

---
+++ success_300.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'b9e3924f990fd1ac72ea1bd96abbc576',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://writtenanonymousgum.com/b9e3924f990fd1ac72ea1bd96abbc576/invoke.js"></scr' + 'ipt>');
---
+++ success_728.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '2eddfdd64abd2b0517aaa47f2223cabe',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://writtenanonymousgum.com/2eddfdd64abd2b0517aaa47f2223cabe/invoke.js"></scr' + 'ipt>');
