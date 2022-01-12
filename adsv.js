aea18200959b630ffca876ce0d970a0b4646109d
mooncero <97421668+mooncero@users.noreply.github.com>
2022-01-12 11:43:42 +0700

    Update adsv.js

--- adsv.js
+++ adsv.js
@@ -1,13 +1,32 @@
-$.ajax({
-  url: '//cdn.jsdelivr.net/gh/mooncero/banner@main/url.txt',
-  success: function(text) {
-    var fileList = text.split('\n');
-    var randomIndex = randomInt(0, fileList.length - 1);    
+var lines;
+var randomNumber;
+var lastRandomNumber;
 
-    $('iframe').attr('src', fileList[randomIndex]);
-  }
+$(document.body).ready(function () {
+
+  // load the trivia from the server
+  $.ajax({
+    url: '//cdn.jsdelivr.net/gh/mooncero/banner@main/url.txt'
+  }).done(function(content) {
+
+    // normalize the line breaks, then split into lines
+    lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
+
+    // only set up the click handler if there were lines found
+    if (lines && lines.length) {
+      $('#showLine').on('click', function () {
+        // loop to prevent repeating the last random number
+        while (randomNumber === lastRandomNumber) {
+          randomNumber = parseInt(Math.random() * lines.length);
+          // check to prevent infinite loop
+          if (lines.length === 1) { break; }
+        }
+        // keep track of the last random number
+        lastRandomNumber = randomNumber;
+
+        // show the corresponding line
+        $('#trivia').text(lines[randomNumber]);
+      });
+    }
+  });
 });
-
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
