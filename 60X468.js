b3e8b7591d2ffff35f482c584ca06f16688bee7c
mooncero <97421668+mooncero@users.noreply.github.com>
2022-12-05 16:29:57 +0700

    Create 60X468.js

---
+++ 60X468.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '01ba7007dc7492889adb28d4fa3ef801',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://embarrasschill.com/01ba7007dc7492889adb28d4fa3ef801/invoke.js"></scr' + 'ipt>');
