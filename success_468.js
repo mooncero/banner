cb754d075ab764975caf122d6f4e75d82e4b49d8
mooncero <97421668+mooncero@users.noreply.github.com>
2022-12-05 16:14:35 +0700

    Delete success_468.js

--- success_468.js
+++
@@ -1,8 +0,0 @@
-	atOptions = {
-		'key' : '0bf67c45867c15ee812fa8ec83000a88',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://writtenanonymousgum.com/0bf67c45867c15ee812fa8ec83000a88/invoke.js"></scr' + 'ipt>');
