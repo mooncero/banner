6ed05ba8b8bb495d0dcd8c06eb51ecc1574acd60
mooncero <97421668+mooncero@users.noreply.github.com>
2022-12-05 16:28:48 +0700

    Create 90x728.js

---
+++ 90x728.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '32b9e14e3431ab388585fc9a9c85fea2',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://embarrasschill.com/32b9e14e3431ab388585fc9a9c85fea2/invoke.js"></scr' + 'ipt>');
